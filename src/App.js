14ae98a1564fd1feeb5a92c1e42464db9eb12451
AhmedDiaab <ahmed_20171801@fci.helwan.edu.eg>
2023-10-28 00:37:35 +0200

    feat(): add routing for meetup pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavouritesPage from "./pages/Favourites";
+import NewMeetupPage from "./pages/NewMeetup";
+
 function App() {
-  return <div> hello world </div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" exact={true} element={<AllMeetupsPage />} />
+        <Route path="/favourites" element={<FavouritesPage />} />
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
