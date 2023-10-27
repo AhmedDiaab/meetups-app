6f00dcd9cef9f5a0bf7acd4a94c84e4e6e4574c8
AhmedDiaab <ahmed_20171801@fci.helwan.edu.eg>
2023-10-28 01:31:11 +0200

    feat(): create and use layout and its styling

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavouritesPage from "./pages/Favourites";
 import NewMeetupPage from "./pages/NewMeetup";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path="/" exact={true} element={<AllMeetupsPage />} />
         <Route path="/favourites" element={<FavouritesPage />} />
         <Route path="/new-meetup" element={<NewMeetupPage />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
