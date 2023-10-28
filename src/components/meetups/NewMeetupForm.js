d08a3c438555217816bb9e00cadbe70dace3d886
AhmedDiaab <ahmed_20171801@fci.helwan.edu.eg>
2023-10-28 14:41:53 +0200

    feat(): saving meetup data to firebase

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import Card from "../ui/Card";
 import classes from "./NewMeetupForm.module.css";
 
-function NewMeetupForm() {
+function NewMeetupForm(props) {
   const titleInputRef = useRef();
   const imageInputRef = useRef();
   const addressInputRef = useRef();
@@ -18,7 +18,7 @@ function NewMeetupForm() {
       description: descriptionInputRef.current.value,
     };
 
-    console.log(meetupData);
+    props.onAddMeetup(meetupData);
   }
 
   return (
