9f5383d360bc82f828f5f7b33462c1f5dff7972c
AhmedDiaab <ahmed_20171801@fci.helwan.edu.eg>
2023-10-28 22:57:30 +0200

    feat(): create context to handle meetups state

---
+++ src/store/favourites-context.js
@@ -0,0 +1,43 @@
+import { createContext, useState } from "react";
+
+const FavouritesContext = createContext({
+  favourites: [],
+  totalFavourites: 0,
+});
+
+function FavouritesContextProvider(props) {
+  const [userFavourites, setUserFavourites] = useState([]);
+
+  function addFavouriteHandler(favouriteMeetup) {
+    // because we depend in a previous version of state
+    // if we use this `prevUserFavouritesState.concat(favouriteMeetup)`
+    // this will cause problem later because we add and depend on
+    // previous state
+    setUserFavourites((prevUserFavouritesState) =>
+      prevUserFavouritesState.concat(favouriteMeetup)
+    );
+  }
+
+  function removeFavouriteHandler(meetupId) {
+    setUserFavourites((prevUserFavouritesState) =>
+      prevUserFavouritesState.filter((meetup) => meetup.id !== meetupId)
+    );
+  }
+
+  function itemIsFavouriteHandler(meetupId) {
+    return userFavourites.some((meetup) => meetup.id === meetupId);
+  }
+
+  const context = {
+    favourites: userFavourites,
+    totalFavourites: userFavourites.length,
+  };
+
+  return (
+    <FavouritesContext.Provider value={context}>
+      {props.children}
+    </FavouritesContext.Provider>
+  );
+}
+
+export default FavouritesContextProvider;
