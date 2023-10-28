c0fd4260577e06d92172ad4029dd3b14fd012839
AhmedDiaab <ahmed_20171801@fci.helwan.edu.eg>
2023-10-28 23:32:30 +0200

    feat(): listing items in favourites page

--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,5 +1,23 @@
+import { useContext } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+import FavouritesContext from "../store/favourites-context";
+
 function FavouritesPage() {
-    return (<div>Favourites Page</div>)
+  const favouritesCtx = useContext(FavouritesContext);
+console.log(favouritesCtx.favourites)
+  let content;
+  if (favouritesCtx.totalFavourites === 0) {
+    content = <p>There is no favourites, you know you can add some!</p>;
+  } else {
+    content = <MeetupList meetups={favouritesCtx.favourites} />;
+  }
+
+  return (
+    <section>
+      <h1>My Favourites</h1>
+      {content}
+    </section>
+  );
 }
 
 export default FavouritesPage;
